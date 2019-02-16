c03d3d5d0aadc4aaa1660485fc5e6cdcb5be16eb
daniel 'kirplink' reynolds <=>
2019-02-16 13:33:14 -0600

    Updates

---
+++ precache-manifest.db72994eb4376190719ec4703352734f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "435a81855f49d55acd25",
+    "url": "/memory-game/static/js/runtime~main.435a8185.js"
+  },
+  {
+    "revision": "03b8bb97367c18b7d0cf",
+    "url": "/memory-game/static/js/main.03b8bb97.chunk.js"
+  },
+  {
+    "revision": "45c63adf297ccdd49609",
+    "url": "/memory-game/static/js/2.45c63adf.chunk.js"
+  },
+  {
+    "revision": "45c63adf297ccdd49609",
+    "url": "/memory-game/static/css/2.dd03191b.chunk.css"
+  },
+  {
+    "revision": "ba170fd755309d952165018f2ca46df7",
+    "url": "/memory-game/index.html"
+  }
+];
